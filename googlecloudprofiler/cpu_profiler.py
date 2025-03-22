3a1af525867bfc87bb3174d2dad7b2dc34bd0f3a
Jatin Agarwala <71180420+jatinAgarwala@users.noreply.github.com>
2025-03-22 22:49:06 +0530

    [DO NOT MERGE] PoC for memory profiling

--- googlecloudprofiler/cpu_profiler.py
+++ googlecloudprofiler/cpu_profiler.py
@@ -11,14 +11,45 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""CPU time profiler."""
+"""Latest working version of Memory profiler."""
 
 import logging
 from googlecloudprofiler import _profiler
 from googlecloudprofiler import builder
+from googlecloudprofiler import profile_pb2
+import memray
+from memray import FileReader
+from memray.reporters.flamegraph import FlameGraphReporter
+import collections
+import time
 
 logger = logging.getLogger(__name__)
 
+def traverse(node_index, nodes, strings, current_stack, traces):
+    """
+    Recursively traverse the tree, accumulating the full call stack.
+    For each leaf node, record the complete call chain with its sample count.
+    """
+    # Only add frames for nodes that represent an actual frame (nonzero name).
+    if nodes['name'][node_index] != 0:
+        frame = (
+            strings[nodes['name'][node_index]],
+            strings[nodes['filename'][node_index]],
+            nodes['lineno'][node_index],
+        )
+        current_stack.append(frame)
+
+    # If this node is a leaf, record the complete stack.
+    if not nodes['children'][node_index]:
+        traces[tuple(current_stack)] += nodes['value'][node_index]
+    else:
+        # Otherwise, traverse each child.
+        for child in nodes['children'][node_index]:
+            traverse(child, nodes, strings, current_stack, traces)
+
+    # Pop the current frame if it was added.
+    if nodes['name'][node_index] != 0:
+        current_stack.pop()
 
 class CPUProfiler:
   """CPU time profiler.
@@ -27,26 +58,51 @@ class CPUProfiler:
   a gzip-compressed profile proto.
   """
 
-  def __init__(self, period_ms=10):
+  def __init__(self, period_ms=10, dump_file_name='some_output_file.bin'):
     """Constructs the CPU time profiler.
 
     Args:
       period_ms: An optional integer specifying the sampling interval in
-        milliseconds. Defaults to 10.
+      milliseconds. Defaults to 10.
     """
     self._profile_type = 'CPU'
     self._period_ms = period_ms
+    self._dump_file_name = dump_file_name
 
   def profile(self, duration_ns):
-    """Profiles the CPU time usage for the given duration.
-
-    Args:
-      duration_ns: An integer specifying the duration to profile in nanoseconds.
+    """Profiles the maximum Memory usage since the application started.
 
     Returns:
-      A bytes object containing gzip-compressed profile proto.
+    A bytes object containing gzip-compressed profile proto.
     """
-    traces = self._profile(duration_ns)
+    start_time = time.time()
+
+    # This requires that the program is run with memray and the bin output file is named "some_output_file.bin"
+    with FileReader(self._dump_file_name, report_progress=True) as reader:
+        snapshot = reader.get_leaked_allocation_records()
+        memory_records = tuple(reader.get_leaked_allocation_records())
+        reporter = FlameGraphReporter.from_snapshot(
+            snapshot,
+            memory_records=memory_records,
+            native_traces=False,
+            inverted=True,
+        )
+
+    Func = collections.namedtuple('Func', ['name', 'filename'])
+    Loc = collections.namedtuple('Loc', ['func_id', 'line_number'])
+
+
+    # Convert memray data to pprof format
+    traces = collections.defaultdict(int)
+
+    nodes = reporter.data["nodes"]
+    strings = reporter.data["strings"]
+    
+    # Start traversal at the root (assumed to be node 0).
+    traverse(0, nodes, strings, [], traces)
+
+    print("Time taken to profile memory usage --- %s seconds ---" % (time.time() - start_time))
+
     return self._build_profile(duration_ns, traces)
 
   def _profile(self, duration_ns):
@@ -54,6 +110,6 @@ class CPUProfiler:
 
   def _build_profile(self, duration_ns, traces):
     profile_builder = builder.Builder()
-    profile_builder.populate_profile(traces, self._profile_type, 'nanoseconds',
-                                     self._period_ms * 1000 * 1000, duration_ns)
+    profile_builder.populate_profile(traces, self._profile_type, 'bytes',
+                                      1, 0)
     return profile_builder.emit()
